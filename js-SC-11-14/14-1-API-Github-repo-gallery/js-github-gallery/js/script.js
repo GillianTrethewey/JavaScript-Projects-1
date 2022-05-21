a397c01236dd53889c5e10d24ebc55a3c6c1fbf5
Gillian Trethewey <cognitiveresults@rogers.com>
2022-05-21 16:24:23 -0400

    step 2

--- js-SC-11-14/14-1-API-Github-repo-gallery/js-github-gallery/js/script.js
+++ js-SC-11-14/14-1-API-Github-repo-gallery/js-github-gallery/js/script.js
@@ -1,7 +1,8 @@
 const overview = document.querySelector("div.overview");
 const username = "GillianTrethewey";
 const url = `https://api.github.com/users/${username}`;
-//const ul = document.querySelector("ul.repo-list");
+const repoList = document.querySelector(".repo-list");
+const url2 = `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`;
 
 const gitUserInfo = async function() {
     const userInfo = await fetch(url);
@@ -26,13 +27,21 @@ const displayUserInfo = function(data) {
     </div> 
     `;
     overview.append(div);
+    gitRepos();
 };
 
-//userInfo(data);
-//     const repos = await data.repos_url;
-// for (let item of repos) {
-//     console.log(item.name);
-//     const li = document.createElement("li");
-//     li.innerHTML = `<a href=${item.html_url}>${item.name}</a>`;
-//     ul.append(li);
-// }
+const gitRepos = async function () {
+    const fetchRepos = await fetch(url2);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+}
+
+const displayRepos = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
+
